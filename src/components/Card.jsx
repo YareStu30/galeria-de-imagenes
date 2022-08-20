98ddc85812ee513b3e20eef4f1c675a84c63eaad
yarenla <yare.cs22@gmail.com>
2022-08-19 22:14:52 -0400

    add components, cards, img, footer and header

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+const Card = (props) => {
+    return (
+      <>
+        <div className="img">
+          <img src={props.src} alt={props.alt}></img>
+          <h4>{props.name}</h4>
+          <h6>{props.category}</h6>
+        </div>
+      </>
+    );
+  };
+  export default Card;
